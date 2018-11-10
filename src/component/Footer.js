71afb200db151df67d1119ba0c9049b18950b9e8
Kyle Stockmann <kyle_stockmann@yahoo.com>
2018-11-09 19:29:51 -0800

    added links

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 
 class Footer extends Component {
+  //EMAIL, GITHUB, ANGELSLIST, and LINKDIN
   render() {
     return (
       <div className="hero-foot">
         <nav className="tabs is-boxed is-fullwidth">
           <div className="container">
             <ul>
-              <li className="is-active"><a>Overview</a></li>
-              <li><a>Modifiers</a></li>
-              <li><a>Grid</a></li>
-              <li><a>Elements</a></li>
+              <li><a>
+                <span className="icon">
+                  <i className="fab fa-github"></i>
+                </span>
+                Github</a></li>
+              <li><a>
+                <span className="icon">
+                  <i className="fab fa-yahoo"></i>
+                </span>
+                Email</a></li>
+              <li><a>
+                <span className="icon">
+                  <i className="fab fa-linkedin"></i>
+                </span>
+                Linkedin</a></li>
+              <li><a>
+                <span className="icon">
+                  <i className="fab fa-angellist"></i>
+                </span>
+              Angel List</a></li>
               <li><a>Components</a></li>
-              <li><a>Layout</a></li>
             </ul>
           </div>
         </nav>
