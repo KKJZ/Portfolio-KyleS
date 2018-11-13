599f36db22b8f1438b04f23d9e4377ca2fd00ff1
Kyle Stockmann <kyle_stockmann@yahoo.com>
2018-11-13 15:16:28 -0800

    added build

---
+++ build/precache-manifest.4bbe0f96c1712a6f64dd14fa713392f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "81a8394d4174501650b7",
+    "url": "/static/js/main.81a8394d.chunk.js"
+  },
+  {
+    "revision": "aae35e431882a55c9aed",
+    "url": "/static/js/1.aae35e43.chunk.js"
+  },
+  {
+    "revision": "81a8394d4174501650b7",
+    "url": "/static/css/main.62ff57ff.chunk.css"
+  },
+  {
+    "revision": "aae35e431882a55c9aed",
+    "url": "/static/css/1.e37751db.chunk.css"
+  },
+  {
+    "revision": "c23375782613248ca4f9f3b2392b44e7",
+    "url": "/index.html"
+  }
+];
