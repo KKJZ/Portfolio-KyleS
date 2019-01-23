ba6d3792c1527c391a74109d14a113ff0140e621
Kyle Stockmann <kyle_stockmann@yahoo.com>
2019-01-22 17:18:15 -0800

    start fresh

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import {createStore, combineReducers} from 'redux';
-import navReducer from './reducers/navReducer';
-
-export default createStore(
-	combineReducers({
-		navbar: navReducer
-	}),
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
