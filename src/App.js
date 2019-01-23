87e5ab54f62230dc3a6cce009927e27e479ec0dd
Kyle Stockmann <kyle_stockmann@yahoo.com>
2019-01-22 18:00:45 -0800

    Setup

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+        </div>
+        <div className="container">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+        </div>
+        <div className="container">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
