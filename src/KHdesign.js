1be2539af760d695ba9ae4bab1a712c827016cc0
Kyle Stockmann <kyle_stockmann@yahoo.com>
2019-02-06 15:09:23 -0800

    changing direction

---
+++ src/KHdesign.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class App extends Component {
+
+    render() {
+        return (
+        <h1 className="title">test</h1>
+        )
+    }
+}
+
+export default App;
