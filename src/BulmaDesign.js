f4554671fc2e3d14a46a2bf98fa22723291365bd
Kyle <kyle_stockmann@yahoo.com>
2019-02-09 21:38:53 -0800

    changing idea

---
+++ src/BulmaDesign.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+// fullscreen hero like original profolio
+// can acutally use that as a good start
+// on scroll the bottom slides up or down accordingly
+// transitions for the inner content
+// background?? forest?? park?? me?? change every screen??
+// green screen photos and edit them in gimp??
+// manga scroll animation
