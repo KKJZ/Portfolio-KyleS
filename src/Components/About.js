1a65bda56f0e9efa26e6c57b48032aa36e02491a
Kyle Stockmann <kyle_stockmann@yahoo.com>
2019-02-13 17:30:06 -0800

    Landing Working

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,13 +6,20 @@ export default function About(props) {
     {/* <!-- Hero content: will be in the middle --> */}
     <a name="About" />
         <div class="hero-body">
-            <div class="container has-text-centered">
-                <h1 class="title">
-                    About Me
-                </h1>
-                <h2 class="subtitle">
-                    Subtitle
-                </h2>
+            <div class="container">
+                <h1 class="title">About Me:</h1>
+                <div class="columns">
+                    <div class="column is-half">
+
+                    </div>
+                    <div class="column is-half">
+                        <section class="subtitle">
+                            <p class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt eleifend risus in lobortis. Vivamus non enim eget nisl ultricies lobortis sit amet a diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris condimentum, mi ac imperdiet viverra, libero odio porttitor arcu, sed commodo sapien sapien quis nibh. Suspendisse lobortis nisi nulla, eget finibus augue faucibus nec.</p>
+                            <p class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt eleifend risus in lobortis. Vivamus non enim eget nisl ultricies lobortis sit amet a diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris condimentum, mi ac imperdiet viverra, libero odio porttitor arcu, sed commodo sapien sapien quis nibh. Suspendisse lobortis nisi nulla, eget finibus augue faucibus nec.</p>
+                            <p class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt eleifend risus in lobortis. Vivamus non enim eget nisl ultricies lobortis sit amet a diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris condimentum, mi ac imperdiet viverra, libero odio porttitor arcu, sed commodo sapien sapien quis nibh. Suspendisse lobortis nisi nulla, eget finibus augue faucibus nec.</p>
+                        </section>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,17 +1,21 @@
 import React from "react";
+import "../css/mystyles.css";
 
 export default function Home(props) {
     return (
-    <section class="hero is-success is-bold is-fullheight">
+    <section class="hero is-success is-fullheight landing">
     <a name="Home" />
     {/* <!-- Hero content: will be in the middle --> */}
         <div class="hero-body">
-            <div class="container has-text-centered">
+            <div class="container">
                 <h1 class="title">
-                    Landing
+                    Hello World!
                 </h1>
                 <h2 class="subtitle">
-                    Subtitle
+                    I am a Full Stack Developer Living in Portland, Oregon
+                </h2>
+                <h2 class="subtitle">
+                    Currently looking for new challenges!
                 </h2>
             </div>
         </div>
